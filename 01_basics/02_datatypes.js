caa8ba1a552861aa49631c71596b57be9fd74768
Aftermath2605 <jhalakprasaddas@gmail.com>
2026-09-24 07:52:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version
+
+// alert("hello"); // we are using node js, not browser
+
+console.log(3+
+    3);  //code readability should be high
+
+
+console.log("jhalak")
+
+// primitive datatypes
+
+let name = "jhalak";
+let age = "23";
+let isLoggedIn = true/false
+
+//number=> 2 to the power 53
+//bigint =>use for big number
+//string => ""
+// boolean=> true/false
+// null=> standlone value (empty)
+// undefined=> value donot defiend
+// symbol=> unique
+
+
+Object
+console.log(typeof undefined) // output= undefiend
+console.log(typeof null) // output= object
